04b99267c80ab437e62044136e84013699c63cf8
Didier Peran Ganthier <didierganthierperan@gmail.com>
2025-02-19 18:43:07 -0500

    add available items data to script

---
+++ script.js
@@ -0,0 +1,7 @@
+const availableItems = [
+    { id: 1, name: "Apple", price: 1.5 },
+    { id: 2, name: "Banana", price: 0.75 },
+    { id: 3, name: "Orange", price: 1.2 },
+    { id: 4, name: "Grapes", price: 2.5 },
+    { id: 5, name: "Mangoes", price: 3.0 }
+  ];
