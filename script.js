beb4c8935c9426d288c9800e13f98d706703ee57
Didier Peran Ganthier <didierganthierperan@gmail.com>
2025-02-19 18:44:42 -0500

    add more available items to the inventory

--- script.js
+++ script.js
@@ -3,5 +3,10 @@ const availableItems = [
     { id: 2, name: "Banana", price: 0.75 },
     { id: 3, name: "Orange", price: 1.2 },
     { id: 4, name: "Grapes", price: 2.5 },
-    { id: 5, name: "Mangoes", price: 3.0 }
-  ];
+    { id: 5, name: "Mangoes", price: 3.0 },
+    { id: 6, name: "Whey Protein", price: 29.99 },
+    { id: 7, name: "Creatine Monohydrate", price: 19.99 },
+    { id: 8, name: "Shaker Bottle", price: 9.99 },
+    { id: 9, name: "Resistance Bands", price: 14.99 },
+    { id: 10, name: "Gym Gloves", price: 12.99 }
+];
